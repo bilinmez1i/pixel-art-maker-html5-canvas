ad5fdce2eeec7f3006a55578252fb5e6e1047d27
arhillis <andriellerhillis@gmail.com>
2018-03-31 06:57:07 -0500

    Cleaned up the JavaScript file for Part 1

--- Part1/designs.js
+++ Part1/designs.js
@@ -1,11 +1,8 @@
 const canvas = document.getElementById("pixelCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "red";
 ctx.fillRect(20, 20, 150, 100);
-
 
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
@@ -16,23 +13,37 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 drawLine(15, 175, 250, 350);
+
+/*ctx.strokeStyle = "slateGray";
+
+for (let i = 20; i < canvas.width; i += 20) {
+    drawLine(i, 0, i, canvas.height);
+}
+
+for (let i = 20; i < canvas.height; i += 20) {
+    drawLine(0, i, canvas.width, i);
+}*/
+
+
 // Select color input => LATER
 // Select size input => LATER
 
 // When size is submitted by the user, call makeGrid()
-ctx.strokeStyle = "slateGray";
 
-for(let i = 20; i < canvas.width; i+=20){
-    drawLine(i, 0, i, canvas.height);
-}
+function makeGrid(numRows, numCols, color) {
+    // Your code goes here!
+    ctx.strokeStyle = color || "black"; //I added the last part just in case color is undefined
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;
+    let width = canvasWidth / numCols;
+    let height = canvasHeight / numRow;
 
-for(let i = 20; i < canvas.height; i+= 20){
-    drawLine(0, i, canvas.width, i);
-}
+    for (let i = width; i < canvasWidth; i += width) {
+        drawLine(i, 0, i, canvasHeight);
+    }
 
-function makeGrid(numRows, numCols) {
-
-// Your code goes here!
-
+    for (let i = height; i < canvasHeight; i += height) {
+        drawLine(0, i, canvasWidth, i);
+    }
 
 }
