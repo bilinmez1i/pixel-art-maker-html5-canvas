30aacd92763f68e12984fdb6d31ac233fb9ae7c0
arhillis <andriellerhillis@gmail.com>
2018-04-02 22:06:07 -0500

    The JavaScript file for Part 2

--- Part2/designs.js
+++ Part2/designs.js
@@ -1,13 +1,6 @@
 const canvas = document.getElementById("pixelCanvas");
 const ctx = canvas.getContext("2d");
 
-const rowsInput = document.getElementById("rows-input");
-const colsInput = document.getElementById("cols-input");
-const submitBtn = document.getElementById("submit-btn");
-
-ctx.fillStyle = "red";
-ctx.fillRect(20, 20, 150, 100);
-
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -16,20 +9,25 @@ function drawLine(x1, y1, x2, y2) {
     ctx.restore();
 }
 
-drawLine(15, 175, 250, 350);
+// Select color input => 
 
-// Select color input => LATER
-// Select size input => LATER
+// Select size input => 
+const rowsInput = document.getElementById("rows-input");
+const colsInput = document.getElementById("cols-input");
+const submitBtn = document.getElementById("submit-btn");
 
-// When size is submitted by the user, call makeGrid()
+
 
 function makeGrid(numRows, numCols, color) {
     // Your code goes here!
+    ctx.fillStyle = "white";//To clear the canvas
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.strokeStyle = color || "black"; //I added the last part just in case color is undefined
     let canvasWidth = canvas.width;
     let canvasHeight = canvas.height;
     let width = canvasWidth / numCols;
-    let height = canvasHeight / numRow;
+    let height = canvasHeight / numRows;
 
     for (let i = width; i < canvasWidth; i += width) {
         drawLine(i, 0, i, canvasHeight);
@@ -40,3 +38,23 @@ function makeGrid(numRows, numCols, color) {
     }
 
 }
+
+makeGrid(20, 20, "lightGray");
+
+// When size is submitted by the user, call makeGrid()
+submitBtn.onclick = function(event){
+    //alert("clicked!");
+
+    event.preventDefault();
+
+    let numRows = rowsInput.value;
+    let numCols = colsInput.value;
+
+    console.log(typeof numRows);
+    console.log(numRows + 1);
+    console.log("Rows: " + numRows);
+    console.log("Columns: " + numCols);
+
+    makeGrid(numRows, numCols, "lightGray");
+};
+
